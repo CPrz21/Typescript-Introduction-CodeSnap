3f97ed4b03bd8f2c89fabe14c3c65d99d376dc29
CPrz21 <faselona@gmail.com>
2021-01-26 10:18:28 -0600

    type inference

---
+++ src/type-inference.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+//* TSC inferred in the type of the variables with the initial values
+let inferredName = "Carlos Pérez";
+inferredName = "José Perez"
+
+let inferredNumber = 20;
+inferredNumber = 27;
+
+let inferredIsMan = true;
+inferredIsMan = false;
