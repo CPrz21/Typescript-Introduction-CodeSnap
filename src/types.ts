90425c74137f422bea7a0051b97c2451ba703f51
CPrz21 <faselona@gmail.com>
2021-01-29 07:10:38 -0600

    types

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+type StringOrNumber = string | number;
+type StockCount = number;
+
+type shirt = {
+  name: string;
+  numberInstock: number;
+}
+
+let myNewShirt: shirt = {
+  name:"Black Shirt",
+  numberInstock: 2
+}
+
+let numberOfShirtsInStock: StockCount = 500;
+let numberOfJacketsInStock: StockCount = 500;
+
+let typesCityPopulation: StringOrNumber = 90;
+typesCityPopulation = "Nine Million";
+
+
+
